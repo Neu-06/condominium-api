0fd18d853257e62aa7490dae461ad24012cc7c2f
Javier Cruz <321javiercruz@gmail.com>
2025-09-20 13:50:27 -0400

    ruta main de backend

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.shortcuts import redirect
+def redirect_to_admin(request):
+    return redirect('/admin/')
 
 urlpatterns = [
+    path('', redirect_to_admin),
     path('admin/', admin.site.urls),
     path('api/cuenta/', include('apps.cuentas.urls')),
     path('api/', include('apps.residentes.urls')),
